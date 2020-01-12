648cf30e2d70a067371a29d7bff31a44783057eb
SergiyMi <bladeneo@i.ua>
2020-01-12 12:20:03 +0200

    Updates

---
+++ precache-manifest.fe928cb05e1d3410c58323b364521ce8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d79d225ed6489b2c68262803d42299fb",
+    "url": "/goit-react-hw-06-phonebook/index.html"
+  },
+  {
+    "revision": "51b1cec2fc9769003a13",
+    "url": "/goit-react-hw-06-phonebook/static/css/main.9ab7fefa.chunk.css"
+  },
+  {
+    "revision": "0c3311324d9b2931c331",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.4c0f2146.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.4c0f2146.chunk.js.LICENSE"
+  },
+  {
+    "revision": "51b1cec2fc9769003a13",
+    "url": "/goit-react-hw-06-phonebook/static/js/main.0a70b081.chunk.js"
+  },
+  {
+    "revision": "9719fa00745b8fafea01",
+    "url": "/goit-react-hw-06-phonebook/static/js/runtime-main.0c92875d.js"
+  }
+]);
